a90e0b5deb6a00d672e8068cd8431350d1c9c61b
ImmortalTerror <81569750+ImmortalTerror@users.noreply.github.com>
2022-03-15 08:35:23 +1100

    did shit
    still doesn't fucking work :(

--- wordle.py
+++ wordle.py
@@ -1,7 +1,7 @@
 # THIS I A WIP
 
 
-# Moduals
+# Modules
 # ////////////////////////////////////////////////
 import requests
 import random
@@ -16,6 +16,11 @@ from termcolor import colored
 
 # Random word function
 def genWord(length = 5):
+    """Generates a random word of a given length
+    Args: length (int)
+    Returns: string
+    default length is 5"""
+    
     print("Generating word...")
     r = requests.get("https://www.mit.edu/~ecprice/wordlist.10000")
     allWords = r.text.splitlines()
@@ -26,6 +31,9 @@ def genWord(length = 5):
 
 # Function to turn string into list
 def lenlist(string):
+    """Turns a string into a list of characters
+    Args: string
+    Returns: list"""
     wordChars = []
     for i in string:
         wordChars.append(i)
@@ -33,8 +41,14 @@ def lenlist(string):
 
 # Gets characters that are an exact match
 def exactMatch(a, b):
+    """Compares two lists to see if they have any matching letters with the same index
+    Args: a (list), b (list)
+    Returns: string"""
+    # if a or b != type(str):
+    #     raise TypeError("Both arguments must be strings")
+
     if len(a) != len(b):
-        return -1
+        raise ValueError("Strings must be the same length")
 
     for i in range(0, len(a)):
         if a[i] == b[i]:
@@ -50,7 +64,28 @@ def exactMatch(a, b):
     return correct
 
 # Compares two words to see if they have any matching letters
-# figure this shit out later :)
+def parMatch(a, b):
+    """Compares two lists to see if they have any matching letters without the same index
+    Args: a (string), b (string)
+    Returns: string"""
+    if a or b != type(str):
+        raise TypeError("Both arguments must be strings")
+    
+    if len(a) != len(b):
+        raise ValueError("Strings must be the same length")
+
+    correct = ""
+
+    for i in b:
+        for c in range(0, len(a)):
+            if a[c] == i and a[c] != b[c]:
+               correct = correct + colored(a[c], "yellow")
+            elif a[c] != i and a[c] != b[c]:
+                correct = correct + a[c]
+    return correct
+            
+
+
 # ////////////////////////////////////////////////
 
 
