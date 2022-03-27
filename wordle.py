d98a9ed54117e64ce0ffa0dcfa4b60f7c59e1d7c
ImmortalTerror <81569750+ImmortalTerror@users.noreply.github.com>
2022-03-27 23:47:20 +1100

    did stuff but idr what :)

--- wordle.py
+++ wordle.py
@@ -1,111 +1,69 @@
-# THIS I A WIP
-
-
 # Modules
-# ////////////////////////////////////////////////
-import requests
-import random
-import os
+from requests import get
+from random import choice
 from termcolor import colored
-# ////////////////////////////////////////////////
 
 
 
-# Functions
-# ////////////////////////////////////////////////
+# genWord Function
+# Generates random word
+allWords = get("https://www.mit.edu/~ecprice/wordlist.10000").text.splitlines()
 
-# Random word function
-def genWord(length = 5):
-    """Generates a random word of a given length
-    Args: length (int)
-    Returns: string
-    default length is 5"""
+def genWord(length = 0):
+    """Generates random word of given length
+    If no length was given, it will do any length
+    Args: Length (int)
+    Returns: string"""
+
+    correctWords = []
     
-    print("Generating word...")
-    r = requests.get("https://www.mit.edu/~ecprice/wordlist.10000")
-    allWords = r.text.splitlines()
-    word = random.choice(allWords)
-    while len(word) != length:
-        word = random.choice(allWords)
-    return word
+    if length != 0:
+        for word in allWords:
+            if len(word) == length:
+                correctWords.append(word)
+        
+        return choice(correctWords)
+    else:
+        return choice(allWords)
 
-# Function to turn string into list
-def lenlist(string):
-    """Turns a string into a list of characters
-    Args: string
-    Returns: list"""
-    wordChars = []
-    for i in string:
-        wordChars.append(i)
-    return wordChars
 
-# Gets characters that are an exact match
-def exactMatch(a, b):
-    """Compares two lists to see if they have any matching letters with the same index
-    Args: a (list), b (list)
+
+def match(guess, word):
+    """Args: guess (string), word (string)
     Returns: string"""
-    # if a or b != type(str):
-    #     raise TypeError("Both arguments must be strings")
-
-    if len(a) != len(b):
-        raise ValueError("Strings must be the same length")
-
-    for i in range(0, len(a)):
-        if a[i] == b[i]:
-            if i == 0:
-                correct = colored(a[i], "green")
-            else:
-                correct = correct + colored(a[i], "green")
-        else:
-            if i == 0:
-                correct = a[i]
-            else:
-                correct = correct + a[i]
-    return correct
-
-# Compares two words to see if they have any matching letters
-def parMatch(a, b):
-    """Compares two lists to see if they have any matching letters without the same index
-    Args: a (string), b (string)
-    Returns: string"""
-    if a or b != type(str):
+    # checks if args are strings
+    if type(guess) != str or type(word) != str:
         raise TypeError("Both arguments must be strings")
-    
-    if len(a) != len(b):
+
+    # Checks if args are same length
+    if len(guess) != len(word):
         raise ValueError("Strings must be the same length")
 
-    correct = ""
+# this shit still ain't work'n :(
 
-    for i in b:
-        for c in range(0, len(a)):
-            if a[c] == i and a[c] != b[c]:
-               correct = correct + colored(a[c], "yellow")
-            elif a[c] != i and a[c] != b[c]:
-                correct = correct + a[c]
-    return correct
-            
+    # for i in range(0, len(guess)):
+    #     if guess[i] == word[i]:
+    #         if i == 0:
+    #             correct = colored(guess[i], "green")
+    #         else:
+    #             correct = correct + colored(guess[i], "green")
+    #     else:
+    #         if i == 0:
+    #             correct = guess[i]
+    #         else:
+    #             correct = correct + guess[i]
+    for i in word:
+        if guess.find(i) != word.find(i):
+            guess = guess.replace(i, colored(i, "yellow"))
+
+    for i in word:
+        if guess.find(i) == word.find(i):
+            guess = guess.replace(i, colored(i, "green"))
+
+    return guess
 
 
-# ////////////////////////////////////////////////
 
-
-
-# Main game
-# ////////////////////////////////////////////////
-
-word = genWord()
-os.system("cls" if os.name == "nt" else "clear")
-
-print(word)
-guess = input()
-
-guessChars = lenlist(guess)
-wordChars = lenlist(word)
-
-print(guessChars)
-print(wordChars)
-
-print(exactMatch(guessChars, wordChars))
-input()
-
-# ////////////////////////////////////////////////
+y = input()
+x = input()
+print(match(x, y))
